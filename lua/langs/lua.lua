33532a0b830f44f5ffa81712260eeb5113b24a04
amirrezaask <raskarpour@gmail.com>
2022-12-01 04:10:45 +0330

    adds null-ls

--- lua/langs/lua.lua
+++ lua/langs/lua.lua
@@ -13,26 +13,8 @@ treesitter.ensure "lua"
 if config(langs, "autoformat", "lua.autoformat") then
   vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*.lua",
-    callback = function(opts)
-      local ok, _ = pcall(require, "plenary")
-      if not ok then
-        return
-      end
-      local buf = opts.buf
-      local Job = require "plenary.job"
-      local j = Job:new {
-        "stylua",
-        "-",
-        writer = vim.api.nvim_buf_get_lines(0, 0, -1, false),
-      }
-      local output = j:sync()
-      if j.code ~= 0 then
-        vim.schedule(function()
-          print "[stylua] Failed to process due to errors"
-        end)
-        return
-      end
-      vim.api.nvim_buf_set_lines(buf, 0, -1, false, output)
+    callback = function(_)
+      vim.lsp.buf.format()
     end,
   })
 end
