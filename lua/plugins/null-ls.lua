4c8b0c6040a9467059bd308fb70d751049716da0
amirrezaask <raskarpour@gmail.com>
2022-12-01 04:30:14 +0330

    remove unknown sources

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -4,13 +4,8 @@ plugin {
     require("null-ls").setup {
       sources = {
         require("null-ls").builtins.code_actions.gitsigns,
-        require("null-ls").builtins.code_actions.proselint,
-        require("null-ls").builtins.diagnostics.proselint,
         require("null-ls").builtins.diagnostics.gitlint,
-        require("null-ls").builtins.diagnostics.solhint,
         require("null-ls").builtins.diagnostics.trail_space.with { disabled_filetypes = { "NvimTree" } },
-        require("null-ls").builtins.formatting.prettierd,
-        require("null-ls").builtins.formatting.phpcsfixer,
         require("null-ls").builtins.formatting.stylua,
         require("null-ls").builtins.formatting.goimports,
       },
